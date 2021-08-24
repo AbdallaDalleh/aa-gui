f54202ec1afd1bac45c8d8e186898ee2c5727d23
Abdalla Al-Dalleh <abdalla.ahmad@sesame.org.jo>
2021-08-24 10:22:52 +0300

    Fetched PVs list from the server into the GUI.
    The GUI can now read the list of all PVs from the archiver and display
    it on listBuffer, add and multi-add buttons have been implemented.

--- FormMain.cpp
+++ FormMain.cpp
@@ -6,6 +6,14 @@ FormMain::FormMain(QWidget *parent)
     , ui(new Ui::FormMain)
 {
     ui->setupUi(this);
+
+    this->ui->dtFrom->setDateTime(QDateTime::currentDateTime().addSecs(-3600));
+    this->ui->dtTo->setDateTime(QDateTime::currentDateTime());
+
+    this->network = new QNetworkAccessManager();
+    QObject::connect(network, SIGNAL(finished(QNetworkReply*)), this, SLOT(networkReplyReceived(QNetworkReply*)));
+
+    this->on_btnFetch_clicked();
 }
 
 FormMain::~FormMain()
@@ -13,3 +21,70 @@ FormMain::~FormMain()
     delete ui;
 }
 
+void FormMain::networkReplyReceived(QNetworkReply *reply)
+{
+    if(reply->error())
+       QMessageBox::information(0, "Error", reply->errorString());
+    else
+    {
+        QJsonDocument doc = QJsonDocument::fromJson(reply->readAll());
+        foreach(QJsonValue v, doc.array())
+        {
+            this->pvs.append(v.toString());
+            this->ui->listBuffer->addItem(v.toString());
+        }
+        this->ui->listBuffer->sortItems();
+    }
+}
+
+void FormMain::on_btnFetch_clicked()
+{
+    this->ui->listBuffer->clear();
+    this->pvs.clear();
+    this->request.setUrl(QUrl(REQUEST_PVS));
+    network->get(request);
+}
+
+void FormMain::on_txtSearch_textChanged(const QString &arg1)
+{
+    if(this->pvs.size() <= 0)
+        return;
+
+    if(arg1.length() == 0)
+        this->on_btnFetch_clicked();
+
+    this->ui->listBuffer->clear();
+    foreach (QString pv, this->pvs) {
+        if(searchMatch(pv, arg1))
+            this->ui->listBuffer->addItem(pv);
+    }
+}
+
+bool FormMain::searchMatch(QString pv, QString arg)
+{
+    if(this->ui->rbStartsWith->isChecked())
+        return pv.startsWith(arg);
+    else if(this->ui->rbEndsWith->isChecked())
+        return pv.endsWith(arg);
+    else if(this->ui->rbWildcard->isChecked())
+    {
+        QRegExp regex;
+        regex.setPattern(arg);
+        regex.setPatternSyntax(QRegExp::Wildcard);
+        return regex.exactMatch(pv) || pv.contains(arg);
+    }
+
+    return false;
+}
+
+void FormMain::on_btnAdd_clicked()
+{
+    this->ui->listData->addItem( this->ui->listBuffer->selectedItems()[0]->text() );
+}
+
+void FormMain::on_btnAddAll_clicked()
+{
+    foreach(QListWidgetItem* item, this->ui->listBuffer->selectedItems()) {
+        this->ui->listData->addItem(item->text());
+    }
+}
--- FormMain.h
+++ FormMain.h
@@ -2,6 +2,17 @@
 #define FORMMAIN_H
 
 #include <QMainWindow>
+#include <QMessageBox>
+#include <QtNetwork/QNetworkAccessManager>
+#include <QtNetwork/QNetworkRequest>
+#include <QtNetwork/QNetworkReply>
+#include <QJsonDocument>
+#include <QJsonArray>
+
+#define SERVER_IP       "10.1.100.9"
+#define RETRIEVAL_PORT  "17668"
+#define MGMT_PORT       "17665"
+#define REQUEST_PVS     "http://" SERVER_IP ":" MGMT_PORT "/mgmt/bpl/getAllPVs?limit=-1"
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class FormMain; }
@@ -15,7 +26,24 @@ public:
     FormMain(QWidget *parent = nullptr);
     ~FormMain();
 
+    bool searchMatch(QString pv, QString arg);
+
+private slots:
+    void networkReplyReceived(QNetworkReply *reply);
+
+    void on_btnFetch_clicked();
+
+    void on_txtSearch_textChanged(const QString &arg1);
+
+    void on_btnAdd_clicked();
+
+    void on_btnAddAll_clicked();
+
 private:
     Ui::FormMain *ui;
+
+    QNetworkAccessManager* network;
+    QNetworkRequest request;
+    QStringList pvs;
 };
 #endif // FORMMAIN_H
